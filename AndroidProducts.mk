d404e6f6ea054ef5a9308d9c962ccf1e26180221
mr-titanic <159266278+mr-titanic@users.noreply.github.com>
2024-05-12 12:29:33 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2018-2019 The risingOS Project
+# Copyright (C) 2018-2019 The infinityOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rising_lavender.mk
+    $(LOCAL_DIR)/infinity_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    rising_lavender-user \
-    rising_lavender-userdebug \
-    rising_lavender-eng
+    infinity_lavender-user \
+    infinity_lavender-userdebug \
+    infinity_lavender-eng
